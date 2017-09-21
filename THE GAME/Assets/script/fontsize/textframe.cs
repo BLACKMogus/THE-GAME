e921e4a748b7c9941eaca3beb64ffb432f8eefb4
BlackMogus <540716134@qq.com>
2017-09-22 00:15:12 +0800

    the 1th day and the gate
    start to design the first day and the most important part of the script

---
+++ THE GAME/Assets/script/fontsize/textframe.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class textframe : MonoBehaviour {
+
+
+    Text tex;
+	// Use this for initialization
+	void Start () {
+        tex = this.gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tex.fontSize = Screen.height / 24;
+	}
+}
---
+++ THE GAME/Assets/script/talkframe.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine.UI;
+public class talkframe : MonoBehaviour {
+    int i = 0;
+    public GameObject mask;
+    public Text input;
+	public enum state
+    {
+        gate,
+        classroom,
+        canteen,
+        playground,
+
+    }
+    public Text lefttext;
+    public Text righttext;
+    public static state map;
+    string[] morning = { "啊~~早啊...", "怎么还不去课室","over" };
+    string[] noon = { "你没睡醒吧...现在是早上啊","over"};
+    string[] evening = { "你没睡醒吧...现在是晚上啊" ,"over"};
+    string[] output;
+
+
+
+
+
+
+
+
+
+	void Start () {
+        map = state.gate;
+        output = morning;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        talk();
+	}
+
+
+    public void talk()
+    {
+        if (map == state.gate)
+        {
+            if (Input.GetMouseButtonDown(0))
+           {
+                if (output[i].Contains("over"))
+                {
+                    mask.SetActive(true);
+                }
+                else
+                {
+                    lefttext.DOText(output[i], 2f, true, ScrambleMode.Custom, " ");
+                    i++;
+                }
+                
+            }
+        }
+
+    }
+    public void dispeararrow()
+    {
+        mask.SetActive(false);
+    }
+
+    public void morningbutton()
+    {
+       if(input.text.Contains("午"))
+        {
+            i = 0;
+          
+            output = noon;
+        }
+       if(input.text.Contains("晚"))
+        {
+            i = 0;
+            output = evening;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
